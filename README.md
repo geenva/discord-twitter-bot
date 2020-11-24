7cdcd2683f0da051ea5aa1b018901ebe70adc82f
geenva <geenva@users.noreply.github.com>
2020-11-24 16:42:51 +0800

    ✨

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+### This package assumes that you already have a Discord bot ready.
+
+THIS IS A FORK OF [Jyrone Parker](https://github.com/mastashake08)'s [original package](https://github.com/mastashake08/discord-twitter-bot).
+
+## Original description:
+
 [![Patreon donate button](https://img.shields.io/badge/patreon-donate-yellow.svg)](https://www.patreon.com/mastashake08)
 [![npm](https://img.shields.io/npm/v/buttplug.svg)](https://npmjs.com/package/discord-twitter-bot)
 [![Discord](https://img.shields.io/badge/Discord-Join%20My%20Server-blue)](https://discord.gg/DMbMmQvvQh)
@@ -6,7 +12,6 @@
 # Discord Twitter Bot
 
 A discord bot that sends messages to a channel whenever a specific user tweets using the Twitter Streaming API. Uses the Twit and Discord.js NPM packages
-
 
 ## Table Of Contents
 
@@ -18,10 +23,11 @@ A discord bot that sends messages to a channel whenever a specific user tweets u
 - [License](#license)
 
 ## Support
+
 Join my Masta Coders Discord server and come join a community of coders, investors and thinkers come join!
 [![CodeLife Join button](https://i.imgur.com/407brBK.png)](https://discord.gg/DMbMmQvvQh)
 
-[You can also support me  via Patreon](http://patreon.com/qdot)!
+[You can also support me via Patreon](http://patreon.com/qdot)!
 Every donation helps us afford more time to create projects and classes for underserved youth!
 
 ## Installation
@@ -45,9 +51,11 @@ node main.js
 ```
 
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
 
 ## License
+
 [ISC](https://choosealicense.com/licenses/isc/)
--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-require('dotenv').config()
-const Twit = require('twit')
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Twit = require("twit");
 
+const channelID = " "; // insert channel id
+const userID = " "; // fill in
 
-var T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
-client.login(process.env.DISCORD_TOKEN);
-client.once('ready', () => {
-  var stream = T.stream('statuses/filter', { follow: [process.env.TWITTER_USER_ID] })
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY, // fill in in .env
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET, // fill in in .env
+  access_token: process.env.TWITTER_ACCESS_TOKEN, // fill in in .env
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, // fill in in .env
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
+});
 
-  stream.on('tweet', function (tweet) {
+client.once("ready", () => {
+  const stream = T.stream("statuses/filter", {
+    follow: ["userID"],
+  });
+
+  stream.on("tweet", function (tweet) {
     //...
-    var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+    const url =
+      "https://twitter.com/" +
+      tweet.user.screen_name +
+      "/status/" +
+      tweet.id_str;
     try {
-        let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
-          channel.send(url)
-        }).catch(err => {
-          console.log(err)
+      let channel = client.channels
+        .fetch(channelID)
+        .then((channel) => {
+          channel.send(url);
         })
+        .catch((err) => {
+          console.log(err);
+        });
     } catch (error) {
-            console.error(error);
+      console.error(error);
     }
-  })
-})
+  });
+});
